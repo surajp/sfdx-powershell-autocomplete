a3b5b64537de376e75a9d7bad6983243ec017a41
Alex Vallat <git@alex.vallat.name>
2020-06-13 08:57:21 +0100

    Keep commands list in memory rather than file on disk

--- sfdx-autocomplete.ps1
+++ sfdx-autocomplete.ps1
@@ -1,20 +1,29 @@
 <# file for storing sfdx commands for faster lookup#>
-$sfdxCommandsFilepath = "$HOME/commands.sfdx"
+$sfdxCommandsFilepath = "$ENV:temp/commands.sfdx"
 
 <# The below script is executed in the background when a new ps session starts to pull all sfdx commands into a file#>
 $sfdxCommandsFileCreateBlock = {
     param($filePath)
     sfdx commands | Out-File "$filePath"
+    $sfdxCommands = @($(Get-Content -Path $filePath))
+    Remove-Item $filePath
+    return $sfdxCommands
 }
 
 <# executes the above script in the background so user is not waiting for the shell to start#>
-Start-Job -ScriptBlock $sfdxCommandsFileCreateBlock -ArgumentList "$sfdxCommandsFilepath" | Out-Null
+$sfdxCommandsFileCreateJob = Start-Job -ScriptBlock $sfdxCommandsFileCreateBlock -ArgumentList "$sfdxCommandsFilepath"
 
 <# script block for autocomplete. looks up matching commands from the file created above#>
 $scriptBlock = {
     param($CommandName, $wordToComplete, $cursorPosition)
 
-    @($(Get-Content -Path $sfdxCommandsFilepath)) -match "$("$wordToComplete".replace('sfdx ','')).*"
+    if (!$sfdxCommands)
+    {
+        $sfdxCommands = Receive-Job -Wait -Job $sfdxCommandsFileCreateJob
+        Remove-Job $sfdxCommandsFileCreateJob
+    }
+
+    $sfdxCommands -match "$("$wordToComplete".replace('sfdx ','')).*"
 }
 <# register the above script to fire when tab is pressed following the sfdx command#>
 Register-ArgumentCompleter -Native -CommandName sfdx -ScriptBlock $scriptBlock
