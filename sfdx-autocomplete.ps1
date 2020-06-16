85522ed8f658a6488be152df67ee77d0bee917e5
Alex Vallat <git@alex.vallat.name>
2020-06-16 18:54:08 +0100

    Various fixes, support for auto-completing parameters as well as commands (#2), support for description of commands and parameters

--- sfdx-autocomplete.ps1
+++ sfdx-autocomplete.ps1
@@ -1,29 +1,44 @@
-<# file for storing sfdx commands for faster lookup#>
-$sfdxCommandsFilepath = "$ENV:temp/commands.sfdx"
+New-Variable -Name sfdxCommands -Scope Script -Force
 
-<# The below script is executed in the background when a new ps session starts to pull all sfdx commands into a file#>
+<# The below script is executed in the background when a new ps session starts to pull all sfdx commands into a variable #>
 $sfdxCommandsFileCreateBlock = {
-    param($filePath)
-    sfdx commands | Out-File "$filePath"
-    $sfdxCommands = @($(Get-Content -Path $filePath))
-    Remove-Item $filePath
-    return $sfdxCommands
+	return sfdx commands --json | ConvertFrom-Json
 }
 
-<# executes the above script in the background so user is not waiting for the shell to start#>
-$sfdxCommandsFileCreateJob = Start-Job -ScriptBlock $sfdxCommandsFileCreateBlock -ArgumentList "$sfdxCommandsFilepath"
+<# executes the above script in the background so user is not waiting for the shell to start #>
+$sfdxCommandsFileCreateJob = Start-Job -ScriptBlock $sfdxCommandsFileCreateBlock
 
-<# script block for autocomplete. looks up matching commands from the file created above#>
+<# script block for autocomplete. looks up matching commands from the file created above #>
 $scriptBlock = {
-    param($CommandName, $wordToComplete, $cursorPosition)
+    param($wordToComplete, $commandAst, $cursorPosition)
 
-    if (!$sfdxCommands)
+    if (!$script:sfdxCommands)
     {
-        $sfdxCommands = Receive-Job -Wait -Job $sfdxCommandsFileCreateJob
+        $script:sfdxCommands = Receive-Job -Wait -Job $sfdxCommandsFileCreateJob
         Remove-Job $sfdxCommandsFileCreateJob
     }
 
-    $sfdxCommands -match "$("$wordToComplete".replace('sfdx ','')).*"
+    if ($commandAst.CommandElements.Count -eq 1) <# List all commands #>
+    {
+        $script:sfdxCommands | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_.id, $_.id, 'Method', $_.description)
+        }
+    }
+    elseif ($commandAst.CommandElements.Count -eq 2 -and $wordToComplete -ne "") <# Completing a command #>
+    {
+        $commandPattern = "^(force:)?" + $commandAst.CommandElements[1].Value + ".+" <# Complete if force: is not specified too #>
+        $script:sfdxCommands | Where-Object id -match $commandPattern | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_.id, $_.id, 'Method', $_.description)
+        }
+    }
+    elseif ($commandAst.CommandElements.Count -gt 2) <# Completing a parameter #>
+    {
+        $parameterToMatch = $commandAst.CommandElements[-1].ToString().TrimStart("-") + "*";
+        
+        ($script:sfdxCommands | Where-Object id -eq $commandAst.CommandElements[1].Value).flags.PsObject.Properties | Where-Object Name -like $parameterToMatch | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new("--" + $_.Value.name, $_.Value.name, 'ParameterName', $_.Value.description)
+        }
+    }
 }
 <# register the above script to fire when tab is pressed following the sfdx command#>
 Register-ArgumentCompleter -Native -CommandName sfdx -ScriptBlock $scriptBlock
