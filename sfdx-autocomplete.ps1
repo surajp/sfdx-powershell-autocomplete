186c85bc55857f08b5d6261415b1ec7149efe097
Suraj Pillai <85.suraj@gmail.com>
2023-04-17 16:22:39 -0400

    Better handling of commands with spaces

--- sfdx-autocomplete.ps1
+++ sfdx-autocomplete.ps1
@@ -15,6 +15,20 @@ $sfdxCommandsFileCreateBlock = {
 <# executes the above script in the background so user is not waiting for the shell to start #>
 $sfdxCommandsFileCreateJob = Start-Job -ScriptBlock $sfdxCommandsFileCreateBlock -argumentlist $global:sfdxCommandsFile
 
+$getFullCommand = {
+    Param($commandAst)
+    # We now have commands separated by spaces instead of colons. Need the loop to figure out where the command ends and the parameters begin
+    [System.Collections.ArrayList]$commandParts = @() 
+    for($i =1; $i -lt ($commandAst.CommandElements.Count);$i++) {
+        if ($commandAst.CommandElements[$i].Value -clike "-*") {
+            break
+        }
+        $commandParts.Add($commandAst.CommandElements[$i].Value) > $null
+    }
+    $commandToMatch = $commandParts -join " "
+    return $commandToMatch.Trim()
+}
+
 <# script block for autocomplete. looks up matching commands from the file created above #>
 $scriptBlock = {
     param($wordToComplete, $commandAst, $cursorPosition)
@@ -27,37 +41,29 @@ $scriptBlock = {
             $script:sfdxCommands = Receive-Job -Wait -Job $sfdxCommandsFileCreateJob
         }
     }
-
     if ($commandAst.CommandElements.Count -eq 1) {
         <# List all commands #>
         $script:sfdxCommands | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_.id, $_.id, 'Method', $_.description??'No description available')
         }
     }
-    elseif ($commandAst.CommandElements.Count -eq 2 -and $wordToComplete -ne "") {
+    elseif ($commandAst.CommandElements.Count -gt 1 -and -Not ($wordToComplete -clike "-*")) {
+        $commandToMatch = (Invoke-Command -ScriptBlock $getFullCommand -ArgumentList $commandAst)
         <# Completing a command #>
-        $commandPattern = ".*" + $commandAst.CommandElements[1].Value + ".*" <# Complete if force: is not specified too #>
+        $commandPattern = ".*" + $commandToMatch + ".*" <# Complete if force: is not specified too #>
         $script:sfdxCommands | Where-Object id -match $commandPattern | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_.id, $_.id, 'Method', $_.description??'No description available')
         }
     }
-    elseif ($commandAst.CommandElements.Count -gt 2) {
-                <# Completing a parameter #>
-        $parameterToMatch = $commandAst.CommandElements[-1].ToString().TrimStart("-") + "*";
-        
-        # We now have commands separated by spaces instead of colons. Need the loop to figure out where the command ends and the parameters begin
-        [System.Collections.ArrayList]$commandParts = @() 
-        for($i =1; $i -lt ($commandAst.CommandElements.Count-1);$i++) {
-            if ($commandAst.CommandElements[$i].Value -clike "-*") {
-                break
-            }
-            $commandParts.Add($commandAst.CommandElements[$i].Value) > $null
-        }
-        $commandToMatch = $commandParts -join " "
+    elseif ($commandAst.CommandElements.Count -gt 2 -and $wordToComplete -clike "-*") {
+        <# Completing a parameter #>
+        $parameterToMatch = $wordToComplete.TrimStart("-") + "*";
+        $commandToMatch = (Invoke-Command -ScriptBlock $getFullCommand -ArgumentList $commandAst)
         ($script:sfdxCommands | Where-Object id -eq $commandToMatch).flags.PsObject.Properties | Where-Object Name -like $parameterToMatch | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new("--" + $_.Value.name, $_.Value.name, 'ParameterName', $_.Value.description??'No description available')
         }
     }
 }
+
 <# register the above script to fire when tab is pressed following the sfdx command#>
 Register-ArgumentCompleter -Native -CommandName sfdx -ScriptBlock $scriptBlock
